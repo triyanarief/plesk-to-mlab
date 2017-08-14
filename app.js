52955b5b548eabbfbe1f5b5cc98280e943bcbccf
triyanarief <triyanarief@icloud.com>
2017-08-14 16:05:18 +0700

    test to serv

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path    = require('path');
+const bodyParser = require('body-parser');
+
+const index   = require('./routes/index');
+const tasks   = require('./routes/tasks');
+
+const app     = express();
+
+// bagian view engine yang merender
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// untuk set statik folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// seting bagian body-parser midle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// routes part
+app.use('/', index);
+app.use('/api', tasks);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,29 +1,4 @@
-const express = require('express');
-const path    = require('path');
-const bodyParser = require('body-parser');
+const app = require('./app');
+const http = require('http');
 
-const index   = require('./routes/index');
-const tasks   = require('./routes/tasks');
-
-const app     = express();
-const port    = 3000;
-
-// bagian view engine yang merender
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-// untuk set statik folder
-app.use(express.static(path.join(__dirname, 'client')));
-
-// seting bagian body-parser midle ware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// routes part
-app.use('/', index);
-app.use('/api', tasks);
-
-app.listen(3000, () => {
-  console.log('server started at port '+port);
-});
+http.createServer(app).listen(process.env.PORT);
